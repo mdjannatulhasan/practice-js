6505b500ecdd94c6d77509f355b6a766f1d1ade0
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-03-09 20:02:53 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 setTimeout(() => {
     console.log("hello");
 }, 3500);
+
+let number = prompt("Enter a number");
+alert(parseInt(number)+200);
+
+if(confirm("hi")){
+    console.log(window.location.href);
+}
